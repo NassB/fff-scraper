c7cb2240c7e34aeda7d2be639ac5297aea33f9fd
Nassim B <bouzouita.nassim@gmail.com>
2024-05-19 22:18:16 +0200

    ⚡️ Supabase implementation
    Updated script to upload json to SQL Table in supabase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -7,17 +8,40 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import os
+from supabase import create_client, Client
+
+load_dotenv()
+
+
+def validate_env_vars():
+    """
+    Checks if the essential environment variables are set.
+    Raises an exception if either SUPABASE_URL or SUPABASE_KEY is missing.
+    """
+    if not os.getenv("SUPABASE_URL") or not os.getenv("SUPABASE_KEY"):
+        raise Exception("Supabase URL or key is not set in environment variables")
+
+
+def create_supabase_client():
+    """
+    Creates and returns a Supabase client configured with the URL and API key from the environment variables.
+    """
+    return create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
 
 
 def fetch_zip_codes():
     """
-    Fetches zip codes from a government API for a specific department.
-    Returns a list of zip codes if successful, an empty list otherwise.
+    Fetches a list of zip codes from a public API for a specified department (38).
+    Returns a list of zip codes if successful; an empty list if the fetch fails.
     """
-    response = requests.get('https://geo.api.gouv.fr/departements/38/communes/')
-    if response.status_code == 200:
+    try:
+        response = requests.get('https://geo.api.gouv.fr/departements/38/communes/')
+        response.raise_for_status()
         return [zip_code for item in response.json() if 'codesPostaux' in item for zip_code in item['codesPostaux']]
-    return []
+    except requests.RequestException as e:
+        print(f"Failed to fetch zip codes: {e}")
+        return []
 
 
 def setup_webdriver():
@@ -29,11 +53,25 @@ def setup_webdriver():
     return webdriver.Chrome(service=service)
 
 
+def upload_data_to_supabase(supabase: Client, data: dict):
+    """
+    Uploads data to the Supabase table "club_logos".
+    Expects data to be a dictionary with club names as keys and logo URLs as values.
+    """
+    table_name = "club_logos"
+    # Prepare the data for batch insertion
+    entries = [{"club_name": k, "logo_url": v} for k, v in data.items()]
+    # Insert data into Supabase
+    supabase.table(table_name).insert(entries).execute()
+
+
 def main():
     """
     Main function to orchestrate the fetching of zip codes, setting up WebDriver, and scraping the club logos.
     Logs club logos to a JSON file and outputs any zip codes that failed to yield results.
     """
+
+    validate_env_vars()
     zip_codes = fetch_zip_codes()
     if not zip_codes:
         print("No zip codes found, exiting.")
@@ -85,11 +123,14 @@ def main():
 
         # Save the collected club logos to a JSON file
         with open('club_logos.json', 'w') as outfile:
-            json.dump(club_logos, outfile, indent=4)
+            json.dump(club_logos, outfile, indent=4, sort_keys=True, ensure_ascii=False)
+
+        client = create_supabase_client()
+        upload_data_to_supabase(client, club_logos)
 
         if failed_zip_codes:
-            print("Failed to find results for the following zip codes:")
-            print(failed_zip_codes)
+            print(f"Failed to find results for the following zip codes ({len(failed_zip_codes)}):")
+            print(sorted(failed_zip_codes))
 
 
 if __name__ == "__main__":
